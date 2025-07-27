950f612c7c4ed6118e763e73d008790ffff084cb
Peter Wilson <git-commit-by@peterwilson.cc>
2025-07-27 12:47:21 +1000

    License under GPL-2.0-or-later.

--- no-fuss-indexnow.php
+++ no-fuss-indexnow.php
@@ -4,8 +4,8 @@
  *
  * @package           NoFussIndexNow
  * @author            Peter Wilson
- * @copyright         YYYY Peter Wilson
- * @license           MIT
+ * @copyright         2025 Peter Wilson, FAIR Contributors
+ * @license           GPL-2.0-or-later
  *
  * @wordpress-plugin
  * Plugin Name: No Fuss IndexNow
@@ -15,8 +15,24 @@
  * Requires PHP: 8.0
  * Author: Peter Wilson
  * Author URI: https://peterwilson.cc
- * License: MIT
+ * License: GPL-2.0-or-later
  * Text Domain: no-fuss-indexnow
+ *
+ *
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
 
 namespace PWCC\NoFussIndexNow;
--- readme.txt
+++ readme.txt
@@ -3,7 +3,7 @@ Contributors: peterwilsoncc
 Tags:
 Tested up to: 6.8
 Stable tag: 1.0.0
-License: MIT
+License: GPL-2.0-or-later
 License URI: https://github.com/peterwilsoncc/no-fuss-indexnow/blob/main/LICENSE
 
 Short Description
