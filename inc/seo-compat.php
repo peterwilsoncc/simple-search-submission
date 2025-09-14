fa31673952067015d42acc21ec354a75cdaf6e0a
Peter Wilson <git-commit-by@peterwilson.cc>
2025-09-14 11:07:40 +1000

    Helper for Yoast SEO to determine noindex status.

--- inc/seo-compat.php
+++ inc/seo-compat.php
@@ -19,13 +19,33 @@ function is_noindex( $post ) {
 		return true; // No post found, treat as noindex.
 	}
 
-
-	if ( class_exists( 'WPSEO_Meta' ) ) {
-		// Yoast SEO.
-		$meta_value = \WPSEO_Meta::get_value( 'meta-robots-noindex', $post->ID );
-		if ( '1' === $meta_value ) {
-			return true;
-		}
+	// Check for Yoast SEO noindex.
+	if ( is_yoast_noindex( $post ) ) {
+		return true;
 	}
 
+	return false; // Default to not noindex.
+}
+
+/**
+ * Check Yoast SEO noindex status.
+ *
+ * @param \WP_Post|int $post The post object or ID.
+ * @return bool True if Yoast SEO sets the post to noindex, false otherwise.
+ */
+function is_yoast_noindex( $post ) {
+	if ( ! function_exists( 'YoastSEO' ) ) {
+		return false; // Yoast SEO is not active.
+	}
+
+	$post    = get_post( $post );
+	$post_id = $post->ID;
+
+	$robots = YoastSEO()->meta->for_post( $post_id )->robots;
+
+	if ( ! isset( $robots['index'] ) ) {
+		return false; // No index directive found.
+	}
+
+	return 'noindex' === $robots['index'];
 }
