b4111c7fe1ee4c7f7632be6c9c99e11bda627f80
Peter Wilson <git-commit-by@peterwilson.cc>
2025-09-14 11:10:26 +1000

    Include SEO Compat functions.

--- simple-search-submission.php
+++ simple-search-submission.php
@@ -39,6 +39,7 @@
 namespace PWCC\SimpleSearchSubmission;
 
 require_once __DIR__ . '/inc/namespace.php';
+require_once __DIR__ . '/inc/seo-compat.php';
 
 // Run late to give themes and plugins a chance to set up asynchronous pings.
 add_action( 'after_setup_theme', __NAMESPACE__ . '\\bootstrap', 20 );
